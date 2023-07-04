b59d5bc912772c09902b9e9c8953d28128e550d9
inung1004 <dksdbswl10430@naver.com>
2023-07-04 19:48:08 +0900

    👀 :: 커뮤니티 페이지 router 추가

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/mainPage";
 import DatailPage from "../pages/detailPage";
+import BoardPage from "../pages/boardPage";
 
 function MainRouter() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/detail/:id" element={<DatailPage />} />
+      <Route path="/comunity" element={<BoardPage />} />
     </Routes>
   );
 }
