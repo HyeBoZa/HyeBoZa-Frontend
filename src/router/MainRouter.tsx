d7e49afba066d7f5e426341776b42485709b6c57
yoon-ho7 <suho0317@gmail.com>
2023-07-04 19:50:37 +0900

    👀 :: 작성 페이지 router 추가

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/mainPage";
 import DatailPage from "../pages/detailPage";
 import BoardPage from "../pages/boardPage";
+import WritePage from "../pages/writePage";
 
 function MainRouter() {
   return (
@@ -9,6 +10,7 @@ function MainRouter() {
       <Route path="/" element={<MainPage />} />
       <Route path="/detail/:id" element={<DatailPage />} />
       <Route path="/comunity" element={<BoardPage />} />
+      <Route path="/write" element={<WritePage />} />
     </Routes>
   );
 }
