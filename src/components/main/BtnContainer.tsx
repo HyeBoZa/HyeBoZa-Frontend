73310027c1d49db1d03d7f40d123149a7ba521fe
inung1004 <dksdbswl10430@naver.com>
2023-07-04 21:36:03 +0900

    📌 :: 컴포넌트 이름 변경

---
+++ src/components/main/BtnContainer.tsx
@@ -0,0 +1,49 @@
+import styled from "@emotion/styled";
+import { RightArrowIcon } from "../../assets";
+import DropDown from "./DropDown";
+import { useNavigate } from "react-router-dom";
+
+interface Props {
+  dropDownResult: string;
+  setDropDownResult: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const BtnContainer = ({ dropDownResult, setDropDownResult }: Props) => {
+  const navigate = useNavigate();
+  return (
+    <Wrapper>
+      <DropDown
+        dropDownResult={dropDownResult}
+        setDropDownResult={setDropDownResult}
+      />
+      <Btn onClick={() => navigate("/comunity")}>
+        <p>커뮤니티</p>
+        <img src={RightArrowIcon} />
+      </Btn>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 13px;
+  width: 960px;
+  justify-content: space-between;
+  position: relative;
+`;
+const Btn = styled.button`
+  width: 150px;
+  height: 50px;
+  border: solid 2px ${({ theme }) => theme.MAIN1};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 18px;
+  border-radius: 16px;
+  font-weight: 700;
+  font-size: 20px;
+  color: ${({ theme }) => theme.MAIN2};
+  background: ${({ theme }) => theme.WHITE};
+`;
+
+export default BtnContainer;
