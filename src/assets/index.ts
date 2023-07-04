e5b4381d9b866bdbf085fcee403677d95d1d34f1
inung1004 <dksdbswl10430@naver.com>
2023-07-04 20:01:24 +0900

    🌱 :: 커뮤니티 로고 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,3 +3,4 @@ export { default as Logo } from "./Logo.png";
 export { default as SearchIcon } from "./SearchIcon.svg";
 export { default as DownArrowIcon } from "./DownArrow.svg";
 export { default as RightArrowIcon } from "./RightArrow.svg";
+export { default as ComunityLogo } from "./ComunityLogo.png";
