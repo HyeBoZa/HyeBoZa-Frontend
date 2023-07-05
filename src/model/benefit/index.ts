afdb17b5c4f584f2c2b7134f7464474aa3b05e40
inung1004 <dksdbswl10430@naver.com>
2023-07-05 10:45:33 +0900

    💡 :: 혜택 상세보기 DTO 설정

--- src/model/benefit/index.ts
+++ src/model/benefit/index.ts
@@ -6,3 +6,12 @@ export interface BenefitContentResponseDto {
   user_category: string;
   benefit_category: string;
 }
+
+export interface DetailBenefitResponseDto {
+  id: number;
+  title: string;
+  control: string;
+  content: string;
+  user_category: string;
+  benefit_category: string;
+}
