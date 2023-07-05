142ce43879e5575706bd092434ec4b9bc0f0f008
inung1004 <dksdbswl10430@naver.com>
2023-07-05 09:34:26 +0900

    📝 :: 카테고리별 혜택 조회 API 연동

--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -4,9 +4,26 @@ import Background from "../../assets/background.png";
 import { useState } from "react";
 import List from "../../components/main/List";
 import BtnContainer from "../../components/main/BtnContainer";
+import { useQuery } from "react-query";
+import { getCategoryBenefit } from "../../utils/apis/benefit";
+import { BenefitContentResponseDto } from "../../model/benefit";
 
 const MainPage = () => {
-  const [dropDownResult, setDropDownResult] = useState<string>("카테고리");
+  const [dropDownResult, setDropDownResult] = useState({
+    name: "어린이",
+    id: "CHILD",
+  });
+  const [benefitList, setBenefitList] = useState<BenefitContentResponseDto[]>();
+
+  const { data } = useQuery(
+    ["benefit_list", dropDownResult],
+    () => getCategoryBenefit(dropDownResult.id),
+    {
+      onSuccess: () => {
+        setBenefitList(data?.benefit_list);
+      },
+    }
+  );
 
   return (
     <Container>
@@ -15,7 +32,7 @@ const MainPage = () => {
         dropDownResult={dropDownResult}
         setDropDownResult={setDropDownResult}
       />
-      <List />
+      <List benefitList={benefitList} />
     </Container>
   );
 };
